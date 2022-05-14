d28478e96050c7d59b1df3df5679bd59cd83366d
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-05-14 17:29:39 +0800

    fix mui errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+// import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Sosmed
+        </Typography>
+        <img src={memories} alt="icon" height="100" width="600" />
+      </AppBar>
+    </Container>
   );
 };
 
