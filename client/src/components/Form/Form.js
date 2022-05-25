b7f3558e70223315c76baabec69890b7dcdee83b
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-05-25 23:05:01 +0800

    fix form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { TextField, Typography, Paper } from "@mui/material";
+import FileBase from "react-file-base64";
 // import useStyles from "./styles.js";
 
 const Form = () => {
   // const classes = useStyles();
-  return <h1>Form</h1>;
+
+  // const handleSubmit = () => {};
+
+  return (
+    <Paper>
+      <form
+        autoComplete="off"
+        noValidate
+        // onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">creating instagram</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          // value={postData.creator}
+          // onChange={}
+        />
+
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          // value={postData.creator}
+          // onChange={}
+        />
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          // value={postData.creator}
+          // onChange={}
+        />
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          // value={postData.creator}
+          // onChange={}
+        />
+        <FileBase type="file" />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
