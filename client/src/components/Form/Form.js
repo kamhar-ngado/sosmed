e553457ec490c45dacfd1954be0699e4914e14cb
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-05-26 00:31:43 +0800

    fix bugs in sosmed

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,55 +1,118 @@
-import React from "react";
-import { TextField, Typography, Paper } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { TextField, Typography, Paper, Button } from "@mui/material";
+// import FileBase from "react-file-base64";
 import FileBase from "react-file-base64";
-// import useStyles from "./styles.js";
+import { createPost } from "../../actions/posts";
+import { useDispatch, useSelector } from "react-redux";
 
-const Form = () => {
-  // const classes = useStyles();
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
 
-  // const handleSubmit = () => {};
+  const post = useSelector((state) =>
+    currentId ? state.posts.find((message) => message._id === currentId) : null
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (post) setPostData(post);
+  }, [post]);
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData));
+  };
 
   return (
     <Paper>
-      <form
-        autoComplete="off"
-        noValidate
-        // onSubmit={handleSubmit}
-      >
-        <Typography variant="h6">creating instagram</Typography>
+      <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Typography variant="h6">
+          {currentId ? `Editing "${post.title}"` : "Creating a Memory"}
+        </Typography>
         <TextField
           name="creator"
           variant="outlined"
           label="Creator"
           fullWidth
-          // value={postData.creator}
-          // onChange={}
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
         />
 
         <TextField
-          name="creator"
+          name="Title"
           variant="outlined"
-          label="Creator"
+          label="Title"
           fullWidth
-          // value={postData.creator}
-          // onChange={}
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
         />
         <TextField
-          name="creator"
+          name="Message"
           variant="outlined"
-          label="Creator"
+          label="Message"
           fullWidth
-          // value={postData.creator}
-          // onChange={}
+          multiline
+          rows={4}
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
         />
         <TextField
-          name="creator"
+          name="Tags"
           variant="outlined"
-          label="Creator"
+          label="Tags"
           fullWidth
-          // value={postData.creator}
-          // onChange={}
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
         />
-        <FileBase type="file" />
+        <FileBase
+          type="file"
+          multiple={false}
+          onDone={({ base64 }) =>
+            setPostData({ ...postData, selectedFile: base64 })
+          }
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
       </form>
     </Paper>
   );
